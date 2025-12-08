6f2bf37fdddb136dba9bd8bc08debe5d7b94777b
Nilanshu Kumar Singh <nilanshukumarsingh2005@gmail.com>
2025-12-08 07:40:33 +0530

    Fix shuffle dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import FlexFiller from './components/FlexFiller'
@@ -6,16 +7,22 @@ import IndexView from './views/IndexView'
 import NotFoundView from './views/NotFoundView'
 import StarBackground from './components/StarBackground'
 
-function App() {
-  const todayUrl = () => {
-    const date = new Date()
-    return `/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
-  }
+const RandomRedirect = () => {
+  const navigate = useNavigate()
 
-  const randomDateUrl = () => {
+  useEffect(() => {
     const start = new Date(1995, 5, 16)
     const end = new Date()
     const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+    navigate(`/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`, { replace: true })
+  }, [navigate])
+
+  return null
+}
+
+function App() {
+  const todayUrl = () => {
+    const date = new Date()
     return `/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
   }
 
@@ -28,7 +35,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Navigate to={todayUrl()} replace />} />
             <Route path="/:year/:month/:day" element={<IndexView />} />
-            <Route path="/random" element={<Navigate to={randomDateUrl()} replace />} />
+            <Route path="/random" element={<RandomRedirect />} />
             <Route path="*" element={<NotFoundView />} />
           </Routes>
         </main>
